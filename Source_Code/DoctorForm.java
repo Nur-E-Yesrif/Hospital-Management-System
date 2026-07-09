41eff7620dd14278d5f36c3af934cb57dfb8358f
Nur-E-Yesrif Samia. <nureyesrifsamia@gmail.com>
2026-07-09 09:11:37 +0600

    Update DoctorForm.java

--- Source_Code/DoctorForm.java
+++ Source_Code/DoctorForm.java
@@ -1,1 +1,43 @@
+package hospitalmanagementsystem;
 
+import javax.swing.*;
+import java.awt.*;
+
+public class DoctorForm extends JFrame {
+
+    JTextField txtName, txtSpecialist;
+    JButton btnSave;
+
+    public DoctorForm() {
+
+        setTitle("Doctor Management");
+        setSize(450,250);
+        setLocationRelativeTo(null);
+
+        JPanel panel = new JPanel(new GridLayout(3,2,10,10));
+
+        panel.add(new JLabel("Doctor Name"));
+        txtName = new JTextField();
+        panel.add(txtName);
+
+        panel.add(new JLabel("Specialization"));
+        txtSpecialist = new JTextField();
+        panel.add(txtSpecialist);
+
+        btnSave = new JButton("Save");
+
+        panel.add(new JLabel());
+        panel.add(btnSave);
+
+        add(panel);
+
+        btnSave.addActionListener(e -> {
+
+            JOptionPane.showMessageDialog(this,
+                    "Doctor Saved Successfully!");
+
+        });
+
+        setVisible(true);
+    }
+}
